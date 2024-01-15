7f19c7d6e32f0ed0e0a4833a7682dfb0213f49fe
krishnachandran-u <kriss52450@gmail.com>
2024-01-16 01:23:40 +0530

    set absolute path instead of relative path main.cpp

--- src/include/DataManip/DataManip.h
+++ src/include/DataManip/DataManip.h
@@ -1,4 +1,4 @@
-#ifndef DATAAMANIP_H
+#ifndef DATAMANIP_H
 #define DATAMANIP_H
 
 #include <iostream>
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,6 @@
 #include "./include/DataManip/DataManip.h"
 
 int main() {
-    DataFrame df = DataFrame("./../datasets/starbucks.csv");   
+    DataFrame df = DataFrame("/home/krishnachandran/myfiles/dev/DataScienceAlgorithms/datasets/starbucks.csv");   
     df.head(10);
 }
